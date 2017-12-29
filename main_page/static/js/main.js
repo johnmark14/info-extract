788b4a18eccf87379c6b7b2356ab42e2dc415822
John Mark <johmarkgabrentina@gmail.com>
2017-12-29 11:27:16 +0800

    Fix bus on resizing

--- main_page/static/js/main.js
+++ main_page/static/js/main.js
@@ -1,7 +1,23 @@
-const CHART = document.getElementById('barchart')
+const BARCHART = document.getElementById('barchart');
+const LINECHART = document.getElementById('linechart');
+const RADARCHART = document.getElementById('radarchart');
 
-let lineChart = new Chart(CHART, {
-        type: 'bar',
+const BARCLASS = document.getElementById("bar-id");
+const LINECLASS = document.getElementById("line-id");
+const RADARCLASS = document.getElementById("radar-id");
+
+const BAR = document.getElementById('bar');
+const LINE = document.getElementById('line');
+const RADAR = document.getElementById('radar');
+
+BAR.onclick = () => {
+
+    BARCLASS.style.display = 'block';
+    LINECLASS.style.display = 'none';
+    RADARCLASS.style.display = 'none;'
+
+    let barChart = new Chart(BARCHART, {
+        type: "bar",
         data: {
             labels: ["CD", "CA", "Donation", "Other"],
             datasets: [{
@@ -26,3 +42,53 @@ let lineChart = new Chart(CHART, {
             }
         }
     });
+}
+
+LINE.onclick = () => {
+
+    LINECLASS.style.display = 'block'
+    BARCLASS.style.display = 'none';
+    RADARCLASS.style.display = 'none';
+
+    let lineChart = new Chart(LINECHART, {
+        type: "line",
+        data: {
+            labels: ["CD", "CA", "Donation", "Other"],
+            datasets: [{
+                label: '#urduja categories stat.',
+                fill: true,
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: "rgba(75, 192, 192, 0.4)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderCapstyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75, 192, 192, 1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
+                pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 3,
+                pointHitRadius: 10
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+BAR.click();
+
+
+
+
