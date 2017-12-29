706f192e177b1594c055ad0337a1a78cbb7f75e2
John Mark <johmarkgabrentina@gmail.com>
2017-12-29 23:28:49 +0800

    Fix some format now ready to do the back end

--- main_page/static/js/main.js
+++ main_page/static/js/main.js
@@ -1,6 +1,7 @@
 const BARCHART = document.getElementById('barchart');
 const LINECHART = document.getElementById('linechart');
 const RADARCHART = document.getElementById('radarchart');
+const DOUGHNUTCHART = document.getElementById('doughnut');
 
 const BARCLASS = document.getElementById("bar-id");
 const LINECLASS = document.getElementById("line-id");
@@ -10,18 +11,29 @@ const BAR = document.getElementById('bar');
 const LINE = document.getElementById('line');
 const RADAR = document.getElementById('radar');
 
+const FEATURE = document.getElementById('feature');
+const ACCURACY = document.getElementById('accuracy');
+const PROBABILITY = document.getElementById('probability');
+
+let category = document.getElementById('category');
+
+//Category
+const CD = "Casualty & Damage";
+const CA = "Caution & Advice";
+const DONATION = "Donation";
+const OTHER = "Other";
+
 BAR.onclick = () => {
 
     BARCLASS.style.display = 'block';
     LINECLASS.style.display = 'none';
-    RADARCLASS.style.display = 'none;'
+    RADARCLASS.style.display = 'none';
 
     let barChart = new Chart(BARCHART, {
         type: "bar",
         data: {
             labels: ["CD", "CA", "Donation", "Other"],
             datasets: [{
-                label: '#urduja categories stat.',
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
                     '#F7474A',
@@ -33,13 +45,25 @@ BAR.onclick = () => {
             }]
         },
         options: {
+            legend: {
+                display: false,
+            },
             scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero:true
                     }
                 }]
-            }
+            },
+            responsive: true,
+            animation: {
+                animationScale: true,
+                animationRotate:true
+            },
+            title: {
+                display: true,
+                text: 'Bar CHart'
+            },
         }
     });
 }
@@ -55,7 +79,6 @@ LINE.onclick = () => {
         data: {
             labels: ["CD", "CA", "Donation", "Other"],
             datasets: [{
-                label: '#urduja categories stat.',
                 fill: true,
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: "rgba(75, 192, 192, 0.4)",
@@ -66,28 +89,90 @@ LINE.onclick = () => {
                 borderJoinStyle: 'miter',
                 pointBorderColor: "rgba(75, 192, 192, 1)",
                 pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
+                pointBorderWidth: 2,
                 pointHoverRadius: 5,
                 pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
                 pointHoverBorderColor: "rgba(220, 220, 220, 1)",
                 pointHoverBorderWidth: 2,
-                pointRadius: 3,
-                pointHitRadius: 10
+                pointRadius: 5,
+                pointHitRadius: 15
             }]
         },
         options: {
+            legend: {
+                display: false,
+            },
             scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero:true
                     }
                 }]
-            }
+            },
+            responsive: true,
+            animation: {
+                animationScale: true,
+                animationRotate:true
+            },
+            title: {
+                display: true,
+                text: 'Line CHart'
+            },
         }
     });
 }
 
+function doughnutStat(d1,d2,d3) {
+
+    let randomScalingFactor = () => {
+        return Math.round(Math.random() * 100);
+    }
+    
+    let doughChart = new Chart(DOUGHNUTCHART, {
+        type: 'doughnut',
+        data: {
+            labels: ['Features', 'Accuracy', 'Probability'],
+            datasets: [{
+                data: [
+                    d1, d2, d3],
+                backgroundColor: [
+                    '#F7474A',
+                    '#FFB55E',
+                    '#48BEBE',
+                ],
+                label: 'Dataset 1'
+            }],
+        },
+        options: {
+            legend: {
+                display: false,
+            },
+            responsive: true,
+            animation: {
+                animationScale: true,
+                animationRotate:true
+            },
+            title: {
+                display: true,
+                text: 'Doughnut CHart'
+            },
+        }
+    });
+
+    FEATURE.style.borderLeft = "2.5em solid #F7474A";
+    FEATURE.style.paddingLeft ="0.5em";
+    ACCURACY.style.borderLeft = "2.5em solid #FFB55E";
+    ACCURACY.style.paddingLeft = "0.5em";
+    PROBABILITY.style.borderLeft = "2.5em solid #48BEBE";
+    PROBABILITY.style.paddingLeft = "0.5em";
+
+    category.innerHTML = CA;
+}
+
 BAR.click();
+doughnutStat(5,0.83,0.63);
+
+
 
 
 
